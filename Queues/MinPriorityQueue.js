517c543c8982da03754803a23194607ca9fb0b6c
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2021-04-21 06:19:44 +0600

    Documented Min Priority Queue

--- Queues/MinPriorityQueue.js
+++ Queues/MinPriorityQueue.js
@@ -2,9 +2,22 @@ const QueueElement = require("./QueueElement");
 
 function MinPriorityQueue() {
     let items = [];
+
+    /**
+     * Returns the length of the Queue.
+     * 
+     * @returns {number}
+     */
     this.size = () => {
         return items.length;
     };
+
+    /**
+     * Adds a new item to the priority queue with the specific priority.
+     * 
+     * @param {any} value 
+     * @param {number} priority 
+     */
     this.enqueue = (value, priority) => {
         const queueElement = new QueueElement(value, priority);
         let added = false;
@@ -21,21 +34,36 @@ function MinPriorityQueue() {
         }
     };
 
+    /**
+     * Removes an item from the front of the queue.
+     * 
+     * @returns {any}
+     */
     this.dequeue = () => {
         const item = items.shift();
         return item? item.value: null;
     };
 
+    /**
+     * Returns the front item of the queue.
+     * 
+     * @returns {any}
+     */
     this.front = () => {
         const item = items[0];
         return item? item.value: null;
     };
 
+    /**
+     * Resets the queue to empty.
+     */
     this.clear = () => {
         items = [];
     };
 
-
+    /**
+     * Prints all the items in the queue.
+     */
     this.print = () => {
         let queueElements = "";
         for(let i = 0; i < this.size(); i++) {
